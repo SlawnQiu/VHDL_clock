0ea3a78abc790502f159d0b9a8ce9860605b5149
saya-kira <slawnqiu@gmail.com>
2020-03-20 00:13:19 +0800

    add two led output pins to entity mode_select

--- source/mode_select.vhd
+++ source/mode_select.vhd
@@ -3,7 +3,8 @@ USE IEEE.std_logic_1164.all;
 
 ENTITY mode_select IS
 PORT(clk_1k,key:in std_logic;
-  mode_out:out std_logic_vector(1 downto 0));
+  mode_out:out std_logic_vector(1 downto 0);
+  led0, led1: out std_logic);
 END mode_select;
 
 ARCHITECTURE behav OF mode_select IS
@@ -25,10 +26,18 @@ PROCESS(clk_1k)
 	BEGIN
 		IF rising_edge(clk_1k) THEN
 			CASE flag IS			--用case语句分别根据flag进入不同的功能
-				WHEN 0 => mode_out <= "00";
-				WHEN 1 => mode_out <= "01";
-				WHEN 2 => mode_out <= "10";
-				WHEN 3 => mode_out <= "11";
+				WHEN 0 => 
+					mode_out <= "00";
+					led0 <= '0'; led1 <= '0';
+				WHEN 1 => 
+					mode_out <= "01";
+					led0 <= '0'; led1 <= '1';
+				WHEN 2 => 
+					mode_out <= "10";
+					led0 <= '1'; led1 <= '0';
+				WHEN 3 => 
+					mode_out <= "11";
+					led0 <= '1'; led1 <= '1';
 			END CASE;
 		END IF;
 END PROCESS;
